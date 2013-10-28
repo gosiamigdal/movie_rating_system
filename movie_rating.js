ff3cca933c658ad94b002e629ca342ee896745da
Malgorzata Migdal <malgorzata@migdal.pl>
2013-10-27 20:10:06 -0700

    Initial version

---
+++ movie_rating.js
@@ -0,0 +1,27 @@
+Movies = new Meteor.Collection("movies");
+// Movies:
+// - name
+// - ratingCount
+// - sumOfRatings
+
+if (Meteor.isClient) {
+  Template.movies.movies = function () {
+    return Movies.find();
+  };
+
+/*
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+*/
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
